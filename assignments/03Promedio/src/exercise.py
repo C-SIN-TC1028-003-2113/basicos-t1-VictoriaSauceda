958bff10b974c3647bb7fe7433f8b7fba465fbef
VictoriaSauceda <a01742388@itesm.mx>
2021-08-16 00:49:16 +0000

    Resolviendo probelmas

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n1 = float(input('Calificación de la materia: '))
+    n2 = float(input('Calificación de la materia: '))
+    n3 = float(input('Calificación de la materia: '))
+    n4 = float(input('Calificación de la materia: '))
+
+    P = (n1 + n2 + n3 + n4) / 4
+
+    print('El promedio es: '+str(P))
 
 
 if __name__ == '__main__':
